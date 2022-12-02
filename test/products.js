db899c75b56fecf499a9343f28530f5085b39d6f
Frangnd <88601717+Frangnd@users.noreply.github.com>
2022-12-02 09:22:07 -0300

    Update products.js

--- test/products.js
+++ test/products.js
@@ -1,4 +1,3 @@
-//During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
@@ -7,9 +6,9 @@ const server = require('../server');
 const should = chai.should();
 
 chai.use(chaiHttp);
-// Our main block
+
 describe('Products', () => {
-  // Consts
+
   const id = '3',
     numProducts = 5,
     successCode = 200,
@@ -21,9 +20,6 @@ describe('Products', () => {
     testName = 'Cannon EOS 80D DSLR Camera',
     testPrice = { title: 'hello', price: '778' };
 
-  /*
-  * Test for /GET
-  */
   describe('/GET product', () => {
     it('it should GET all the products', done => {
       chai.request(server)
@@ -36,9 +32,7 @@ describe('Products', () => {
         });
     });
   });
-  /*
-  * Test for /POST
-  */
+ 
   describe('/POST product', () => {
     it('it should POST a product ', done => {
       chai.request(server)
@@ -55,9 +49,7 @@ describe('Products', () => {
         });
     });
   });
-  /*
-  * Test for /GET:id
-  */
+ 
   describe('/GET/:id product', () => {
     it('it should GET a book by the given id', done => {
       chai.request(server)
@@ -73,9 +65,7 @@ describe('Products', () => {
         });
     });
   });
-  /*
-  * Test for /PUT:id
-  */
+ 
   describe('/PUT/:id product', () => {
     it('it should UPDATE a product given the id', done => {
       chai.request(server)
@@ -92,9 +82,7 @@ describe('Products', () => {
         });
     });
   });
-  /*
-  * Test for /DELETE:id
-  */
+
   describe('/DELETE/:id product', () => {
     it('it should DELETE a product given the id', done => {
       chai.request(server)
